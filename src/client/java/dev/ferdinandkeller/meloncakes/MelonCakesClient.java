1518f5dba9998d9334c824b03e3b48b9255a0d34
Ferdinand Keller <ferdinand.keller@proton.me>
2025-09-12 20:29:09 +0200

    created repo

---
+++ src/client/java/dev/ferdinandkeller/meloncakes/MelonCakesClient.java
@@ -0,0 +1,10 @@
+package dev.ferdinandkeller.meloncakes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MelonCakesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/dev/ferdinandkeller/meloncakes/MelonCakesDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.ferdinandkeller.meloncakes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MelonCakesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
